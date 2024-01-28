87729f410db2494fff1666676187bdbc0d21be4b
ananta khodake <ananta.khodake02@gmail.com>
2024-01-28 13:36:54 +0530

    rearchitect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-import HtmlEditor from "./components/HtmlEditor";
-import CssEditor from "./components/CssEditor";
-import JsEditor from "./components/JsEditor";
-import Editor from "./components/Editor";
-import { useState } from "react";
+import Editor from "./pages/editor/Editor";
+import Trending from "./pages/trending/trending";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout";
 
 function App() {
-  const [htmlCode, setHtmlCode] = useState("");
-  const [cssCode, setCSSCode] = useState("");
-  const [jsCode, setJsCode] = useState("");
-
   return (
-    <>
-      <div className="left">
-        <HtmlEditor
-          code={htmlCode}
-          onChange={(e) => setHtmlCode(e)}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route
+          path="/trending"
+          element={<RootLayout ScreenComponent={<Trending />} />}
         />
-        <CssEditor
-          code={cssCode}
-          onChange={(e) => setCSSCode(e)}
-        />
-        <JsEditor code={jsCode} onChange={(e) => setJsCode(e)} />
-      </div>
-      <div className="right">
-        <Editor codeToRender={`<style>${cssCode}</style> <body>${htmlCode}</body> <script>${jsCode}</script>`} />
-      </div>
-    </>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
